d8ee78325d3e76092965f5afeb57785e10fee521
Reinhardt Rijna <reinhardtrijna@gmail.com>
2020-11-04 15:27:56 +0100

    Search function
    Needs:
    - Callback function when tapping on result.
    - Case insensitivity

--- lib/pages/discover.dart
+++ lib/pages/discover.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:memecentral/variables.dart';
 
 class DiscoverPage extends StatefulWidget {
   @override
@@ -6,8 +8,77 @@ class DiscoverPage extends StatefulWidget {
 }
 
 class _DiscoverPageState extends State<DiscoverPage> {
+  TextEditingController searchController = TextEditingController();
+
+  Future<QuerySnapshot> searchResult;
+  searchUser(String searchQuery) {
+    print("searchQuery:" + searchQuery);
+    var users = userCollection.where('username', isGreaterThanOrEqualTo: searchQuery).get();
+    if (searchQuery != null && searchQuery != "") {
+      setState(() {
+        searchResult = users;
+      });
+    } else {
+      setState(() {
+        searchResult = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: TextFormField(
+          controller: searchController,
+          decoration: InputDecoration(
+            filled: true,
+            prefixIcon: Icon(Icons.search, color: Colors.white),
+            hintText: "Search...",
+            hintStyle: fontStyle(18, Colors.white),
+          ),
+          onChanged: (text) => searchUser(text),
+        ),
+      ),
+      body: searchResult == null
+          ? Center(
+              child: Text(
+                "Search for users...",
+                style: fontStyle(25),
+              ),
+            )
+          : FutureBuilder(
+              future: searchResult,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot user = snapshot.data.docs[index];
+                    return Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.search, color: Colors.white, size: 30),
+                          trailing: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: NetworkImage(user.data()['profilePicture']),
+                          ),
+                          title: Text(
+                            user.data()['username'],
+                            style: fontStyle(22, Colors.white),
+                          ),
+                        ),
+                        Divider(),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+    );
   }
 }
